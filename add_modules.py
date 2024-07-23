865ec1fa5915e93ead9a333a2c386e4f6abb678d
pz522-ic <122883063+pz522-ic@users.noreply.github.com>
2024-07-23 12:51:13 +0100

    Update add_modules.py

--- add_modules.py
+++ add_modules.py
@@ -2,7 +2,6 @@ from db_setup import session
 from module_class import Module  
 
 def add_module(session, name, code, prerequisites, recommended_prerequisites, term, lecturer, assessments):
-    # Convert list fields to strings
     prerequisites_str = ', '.join(prerequisites) if prerequisites else ""
     assessments_str = ', '.join(assessments) if assessments else ""
     
